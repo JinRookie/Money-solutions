83f6665eb32f254977646711c774ec5bc946008e
Jin <jinmwass@gmail.com>
2026-07-28 13:36:07 +0300

    feat: add Logger utility and AppConfig log level

---
+++ js/utils/logger.util.js
@@ -0,0 +1,10 @@
+export const AppConfig = Object.freeze({
+  NAME: 'MoneyManager',
+  VERSION: '1.0.0',
+  DB_VERSION: 1,
+  STORAGE_PREFIX: 'mm_',
+  DEFAULT_CURRENCY: 'QAR',
+  DEFAULT_LOCALE: 'en-QA',
+  DEFAULT_TIMEZONE: 'Asia/Qatar',
+  LOG_LEVEL: 'INFO', // DEBUG, INFO, WARN, ERROR
+});
